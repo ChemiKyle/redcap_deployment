59f1fcfb22ba76b1a8592fe128d478ae781ebd93
Marly Cormar <marlycormar@ufl.edu>
2018-09-28 17:06:10 -0400

    Add function to deploy_module to host.

--- module.py
+++ module.py
@@ -1,10 +1,8 @@
 from fabric.api import *
 import os
 import re
-import utility
 import urllib.request
 
-@task
 def module_exists(module_name, repo_base="ctsit"):
     url = "https://api.github.com/repos/%s/%s" %(repo_base, module_name)
     with hide('output', 'running', 'warnings'):
@@ -13,7 +11,6 @@ def module_exists(module_name, repo_base="ctsit"):
     else: print("The module %s/%s does exist." %(repo_base, module_name))
     return output
 
-@task
 def get_latest_release_tag(module_name, repo_base="ctsit"):
     tag = ""
 
@@ -32,7 +29,6 @@ def get_latest_release_tag(module_name, repo_base="ctsit"):
             print("The tag for module %s/%s is %s." %(repo_base, module_name, tag))
     return tag
 
-@task
 def get_latest_release_zip(module_name, repo_base="ctsit"):
     if(module_exists(module_name, repo_base)):
         tag = get_latest_release_tag(module_name, repo_base)
@@ -40,12 +36,34 @@ def get_latest_release_zip(module_name, repo_base="ctsit"):
             abort("The module %s/%s has not been released yet." %(repo_base, module_name))
 
         url = "https://github.com/%s/%s/archive/%s.zip" %(repo_base, module_name, tag)
-        file_name = "%s_v%s.zip" %(module_name, tag)
-        urllib.request.urlretrieve(url, file_name)
+        file_name = "%s_v%s" %(module_name, tag)
+        urllib.request.urlretrieve(url, file_name + '.zip')
 
         return file_name
     else:
         abort("The module %s/%s doesn't exist." %(repo_base, module_name))
+
+@task
+def deploy_module(module_name, module_version="", repo_base="ctsit"):
+    """
+    Deploy module to the specified host without enabling it.
+    If no module_version is provided the latest version is deployed.
+    """
+
+    file_name = get_latest_release_zip(module_name, repo_base)
+    #file_name = "project_ownership_v1.2.1"
+
+    with settings(user=env.deploy_user):
+    # Make a temp folder to upload the tar to
+        temp_dir = run('mktemp -d')
+        zip_file = file_name + '.zip'
+        put(zip_file, temp_dir)
+        run('unzip %s/%s -d %s' % (temp_dir, zip_file, temp_dir))
+        run('rm %s/*.zip' %(temp_dir))
+        run("mv %s/* %s/%s" %(temp_dir, temp_dir, file_name))
+        run('rsync -rc %s/%s %s/modules' % (temp_dir, file_name, env.live_project_full_path))
+        # Remove the temp directories
+        run('rm -rf %s' % (temp_dir))
 
 @task
 def enable(module_name, module_version="", pid=""):
